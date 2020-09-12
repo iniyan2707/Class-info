b7f082cef8c54360179411e4aba19ef9a8da26a9
iniyan2707 <iniyan.cool3@gmail.com>
2020-09-12 11:44:03 +0530

    Added splash screen and modified ui

---
+++ app/src/main/java/com/example/classinfo/Splashscreen.java
@@ -0,0 +1,49 @@
+package com.example.classinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splashscreen extends AppCompatActivity {
+
+    Animation topAnim,bottomAnim;
+    ImageView imageView;
+    TextView classlogo;
+    private static int SPLASH_SCREEN=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        imageView=(ImageView) findViewById(R.id.logo);
+        classlogo=(TextView) findViewById(R.id.classlogo);
+
+        imageView.setAnimation(topAnim);
+        classlogo.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splashscreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        },SPLASH_SCREEN);
+
+
+    }
+}
